7e11691a9752df481eacdf6c87925309b5d1c98a
vanshYadav <vanshajjyadava@gmail.com>
2026-01-31 17:08:53 +0530

    added request router

---
+++ src/routes/requestRouter.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const requestRouter = express.Router();
+
+requestRouter.post("/sendConnectionRequest", async (req, res) => {});
+
+module.exports = requestRouter;
