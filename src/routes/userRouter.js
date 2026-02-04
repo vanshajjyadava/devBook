0d4bea397fc31fea888279e83701b1c0dd28ca05
vanshYadav <vanshajjyadava@gmail.com>
2026-02-04 15:50:06 +0530

    added GET /user/connections API

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -26,4 +26,32 @@ userRouter.get("/user/requests/recieved", userAuth, async (req, res) => {
   }
 });
 
+// GET API - shows all the connections of a loggedInUser
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connections = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connections.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+
+      return row.fromUserId;
+    });
+
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
+  }
+});
+
 module.exports = userRouter;
